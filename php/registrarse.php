561bd048191268b4bce224a1c0f327e1948dfbb3
Jogua <josegua93@gmail.com>
2015-12-05 19:58:55 +0100

    Conexión BD

--- php/registrarse.php
+++ php/registrarse.php
@@ -1,9 +1,3 @@
 <p>Registrarse</p>
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
