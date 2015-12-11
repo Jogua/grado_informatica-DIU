b38f14794da7a2e903d61e258466f5acf9f9f1a6
Jogua <josegua93@gmail.com>
2015-12-11 13:07:41 +0100

    Nuestras salas

--- php/salas.php
+++ php/salas.php
@@ -1,17 +1,41 @@
 <?php $seccion="salas"; include 'header.php';?>
 
 <div class="container">
+    <h3>Nuestras salas</h3>
+    <?php
+    include 'dbConnect.php';
+    $conn = dbConnect();
 
-<p>Nuestras salas</p>
-<?php
+    $sql = "SELECT * FROM sala;";
+    $resultado = mysqli_query($conn, $sql);
+    $i = 0;
+    while($sala = mysqli_fetch_assoc($resultado)){
+        echo "<hr>";
+        if($i%2 == 0){
+            $clase = "izquierda";
+        }else{
+            $clase = "derecha";
+        }
+        echo '<div class="contenidoSala row">';
+        echo '<img class="salaImg ' . $clase . '" src="../' . $sala['imagen'] . '">';
+        
+        echo '<div class="col-lg-9 col-md-9">';
+        echo '<h4>';
+        echo $sala['nombre'];
+        echo '</h4>';
+        echo '<p>';
+        echo $sala['ubicacion'];
+        echo '</p>';
+        $descripcion = explode("\n", $sala['descripcion']);
+        foreach ($descripcion as $str) {
+            echo "<p>" . $str . "</p>";
+        }
+        echo '</div>';
+        echo '</div>';
+        $i = $i+1;
+    }
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-?>
+    ?>
 </div>
 
 <?php include 'footer.php';?>
