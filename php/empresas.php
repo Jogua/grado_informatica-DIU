6fc0bdc8c22b5dbd4a23fc7dfc14d8a629ee5031
Jogua <josegua93@gmail.com>
2015-12-14 14:04:28 +0100

    empresa

--- php/empresas.php
+++ php/empresas.php
@@ -1,16 +1,45 @@
 <?php $seccion="empresas"; include 'header.php';?>
 
 <div class="container">
-    
-<p>Empresas</p>
-<?php
+  <h3>Nuestras empresas</h3>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+  <?php
+  include 'dbConnect.php';
+  $conn = dbConnect();
+
+  $sql = "SELECT * FROM empresa;";
+  $resultado = mysqli_query($conn, $sql);
+  $i = 0;
+
+  while($empresa = mysqli_fetch_assoc($resultado)){
+    if($i%4 == 0){
+      //$clase = "izquierda";
+      echo "<hr>";
+      echo '<div class="row">';
+    }else{
+      //$clase = "derecha";
+    }
+    echo '<div class="col-lg-3 col-md-3">';
+    echo '<img class="empresaImg" src="../' . $empresa['imagen'] . '">';
+    echo '<h4>';
+    echo $empresa['nombre'];
+    echo '</h4>';
+    echo '<p>';
+    echo $empresa['descripcion'];
+    echo '</p>';
+    echo '<p><a href="http://' . $empresa['web'] . '">';
+    echo $empresa['web'];
+    echo '</a></p>';
+    echo '</div>';
+    $i = $i+1;
+    if($i%4 == 0){
+      echo "</div>"; //div row
+    }
+  }
+  if($i%4 != 0){//por si no hay un numero multimplo de $numEmpresasFila
+    echo "</div>"; //div row
+  }
+
+  ?>
 </div>
-
 <?php include 'footer.php';?>
